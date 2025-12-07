980934cd8ba228403e00ad135a7e83ecefe7da62
surwxa <nepalisurakshya767@gmail.com>
2025-12-07 21:51:20 +0545

    adding a file

--- 1stProject/script.js
+++ 1stProject/script.js
@@ -1,13 +1,54 @@
-
+function loadProduct(){
 fetch('https://fakestoreapi.com/products')
 .then ((res)=>res.json())
 .then ((data)=>{
   const box=document.getElementById('product')
   data.forEach((item)=>{
-    box.innerHTML+=`<div class="products">
-    <img src="${item.image}"/>
+    box.innerHTML+=`<a href="#${item.id}"><div class="products">
+    <img class="product-image" src="${item.image}"/>
     <div class="product-title">${item.title}</div>
     <div class="product-price">${item.price}</div>
-    </div>`
+    </div></a>
+    `
   })
 })
+}
+loadProduct()
+
+function loadProductDetail(id){
+  fetch(`https://fakestoreapi.com/products/${id}`)
+  .then ((res)=>res.json())
+  .then ((data)=>{
+  document.getElementById('productDetail').innerHTML+=`
+  <button onclick="goback()"class="back-btn">Back</button>
+  <div class="detail">
+    <img src="${data.image}" class="detail-img">
+    <div class="detail-info">
+    <h2>${data.title}</h2>
+    <p><strong>Price:</strong>$${package.price}</p>
+    <p>${item.description}</p>
+    </div>
+    </div>
+    `
+    document.getElementById("product").style.display="none"
+    document.getElementById('productDetail').style.display="block"
+  })
+}
+
+function goback(){
+  location.hash="";
+  document.getElementById("product").style.display="none"
+    document.getElementById('productDetail').style.display="block"
+}
+
+window.addEventListener("hashchange",()=>{
+  const id=location.hash.replace("#","")
+  if(id)loadProductDetail(id);
+  else goback();
+});
+
+loadProduct();
+if(location.hash){
+  loadProductDetail(location.hash.replace("#",""))
+}
+loadProductDetail()
