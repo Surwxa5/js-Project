14863cec5c51ffe7497e18d5261b1cd430734b1c
surwxa <nepalisurakshya767@gmail.com>
2026-01-03 17:25:29 +0545

    adding a content

--- 2ndProject/homeProductcards.js
+++ 2ndProject/homeProductcards.js
@@ -1,1 +1,43 @@
-export const showProductContainer=(products)=>{};
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
+const productContainer= document.querySelector('#productContainer');
+const productTemplate=document.querySelector('#productTemplate')
+
+// check if the product exit or not 2nd
+export const showProductContainer=(products)=>{
+  if(!products){
+    return false;
+  }
+
+  products.forEach((curProd)=>{
+    const {category,description,id,image,name,price,stock}=curProd;
+
+    // to clone the content of  template element we have to use importNode
+    
+    const productClone=document.importNode(productTemplate.content,true);
+
+    productClone.querySelector('#cardValue').setAttribute('id',``)
+
+
+    productClone.querySelector('.category').textContent=category;
+    productClone.querySelector('.productName').textContent=name;
+    productClone.querySelector('.productImage').src=image;
+    productClone.querySelector('.productImage').alt=image;
+     productClone.querySelector('.productDescription').textContent=description;
+      productClone.querySelector('.productStock').textContent=stock;
+      productClone.querySelector('.productPrice').textContent=`Rs${price}`;
+      productClone.querySelector('.productActualPrice').textContent=`Rs${price *2}`;
+
+      // from here working on increment and decrement of the button
+      productClone.querySelector(".stockElement")
+      .addEventListener("click",(event)=>{
+        // ctrl and space for automatic space
+        homeQuantityToggle(event,id,stock)
+      })
+
+    productContainer.append(productClone)
+
+  });
+
+
+};
--- 2ndProject/script.js
+++ 2ndProject/script.js
@@ -1,5 +1,6 @@
 import './style.css';
 import products from "./api/products.json";
+import { showProductContainer } from './homeProductcards';
 
 // Define a function names 'showProductContainer' that takes an array of products as input 
 showProductContainer(products)
