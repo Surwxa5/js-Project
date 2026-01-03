bf1c15f37a1eb598b6853f719678af34926a96e8
surwxa <nepalisurakshya767@gmail.com>
2026-01-03 12:44:21 +0545

    adding a file

---
+++ 2ndProject/homeProductcards.js
@@ -0,0 +1,1 @@
+export const showProductContainer=(products)=>{};
---
+++ 2ndProject/script.js
@@ -0,0 +1,5 @@
+import './style.css';
+import products from "./api/products.json";
+
+// Define a function names 'showProductContainer' that takes an array of products as input 
+showProductContainer(products)
