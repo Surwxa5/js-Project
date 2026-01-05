c7c2366e44ff6106b1fe679dc4c1c1510112a749
surwxa <nepalisurakshya767@gmail.com>
2026-01-05 22:44:10 +0545

    working on cart quantity

--- 2ndProject/homeQuantityToggle.js
+++ 2ndProject/homeQuantityToggle.js
@@ -1,5 +1,31 @@
 export const homeQuantityToggle=(event,id,stock)=>{
+  // this is to know which card i had clicked and know all the data of that card
 const currentCardElement=document.querySelector(`#card${id}`)
 
+// accesing the only productquentity data 
 const productQuantity=currentCardElement.querySelector(".productQuantity")
+
+// to work on the quantity 1 and getting the quantity
+
+
+let quantity=parseInt(productQuantity.getAttribute('data-quantity'))|| 1;
+
+if(event.target.className==="cartIncrement"){
+  if(quantity<stock){
+    quantity+=1;
+  }
+  else if(quantity===stock){
+    quantity=stock;
+  }
 }
+
+if(event.target.className==="cartDecrement"){
+  if(quantity>1){
+    quantity-=1;
+  }
+}
+// now we have to set the updated value of quantity
+productQuantity.innerText=quantity;
+productQuantity.setAttribute('data-quantity',quantity)
+return quantity;
+  }
