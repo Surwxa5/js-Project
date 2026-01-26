7ac22eb86e8a0f63563d9c60266720d9bf67ff85
surwxa <nepalisurakshya767@gmail.com>
2026-01-26 21:06:41 +0545

    Localstorage

--- 2ndProject/showAddToCartCards.js
+++ 2ndProject/showAddToCartCards.js
@@ -5,5 +5,32 @@ import { getcartProductFromls } from "./getCartProducts";
 let cartProducts=getcartProductFromls();
 
 let filterProducts=products.filter((curProd)=>{
-console.log(curProd.id)
+ return cartProducts.some((curElem)=>curElem.id===curProd.id) 
+
+}) 
+console.log(filterProducts)
+
+// to update the add to cart lage
+const cartElement=document.querySelector("#productCartContainer")
+const templateContainer=document.querySelector("#productTemplate");
+
+
+const showCartProcuct=()=>{
+filterProducts.forEach((curProd)=>{
+
+  const {category,id,image,name,stock,price}=curProd
+  let productClone=document.importNode(templateContainer.content,true);
+  
+
+   productClone.querySelector('#cardValue').setAttribute('id',`card${id}`)
+  productClone.querySelector('.category').textContent = category;
+   productClone.querySelector('.productName').textContent=name;
+    productClone.querySelector('.productImage').src=image;
+
+  cartElement.append(productClone)
 })
+}
+
+
+// showing the carts Products
+showCartProcuct()
