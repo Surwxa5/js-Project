96b508e2ea0626042301810b7ab129ed826102fa
surwxa <nepalisurakshya767@gmail.com>
2026-01-27 20:35:16 +0545

    cartProduct

--- 2ndProject/updateCart.js
+++ 2ndProject/updateCart.js
@@ -4,3 +4,4 @@ export const updateCart = (cartProducts)=>{
 return (cartValue.innerHTML=`<i class="fa-solid fa-basket-shopping">${cartProducts.length}</i>`)
 }
 
+// i really wanna work but i cannot because my eyes hurt
